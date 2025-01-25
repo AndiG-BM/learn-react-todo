ee34b341251aa20f17835c84d94f238c79536dc8
Andi Goodinson <andig106605@gmail.com>
2025-01-25 14:28:16 +1100

    intitial commit
    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+const DATA = [
+	{ id: "todo-0", name: "eat", completed: true },
+	{ id: "todo=1", name: "sleep", completed: false },
+	{ id: "todo=2", name: "repeat", completed: false },
+];
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App tasks={DATA} />
+	</StrictMode>
+);
